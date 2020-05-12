59f1334b660f06fb5b140b91f4cdc033cacf7e06
Joona Piirainen <joona.piirainen@gmail.com>
2020-05-12 20:05:22 +0300

    second commit

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,8 @@ import { Link } from "gatsby"
 
 export default () => (
   <>
-    <h1>Hello All!</h1>
-    <Link to="/about/">about me &rArr;</Link>
+    <h1>About me</h1>
+    <p>I am Joona Piirainen</p>
+    <Link to="/">&lArr; home</Link>
   </>
 )
