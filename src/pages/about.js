4154a2d38495766f7a140789b7bfc2c66b4edd08
Joona Piirainen <joona.piirainen@gmail.com>
2020-05-13 13:42:03 +0300

    added baseline styling

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,11 @@
 import React from "react"
 import { Link } from "gatsby"
+import Layout from "./components/layout"
 
 export default () => (
-  <>
+  <Layout>
     <h1>About me</h1>
     <p>I am Joona Piirainen</p>
     <Link to="/">&lArr; home</Link>
-  </>
+  </Layout>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from "react"
 import { Link } from "gatsby"
+import Layout from "./components/layout"
 
 export default () => (
-  <>
+  <Layout>
     <h1>Hello All!</h1>
+    <p>Good morning people</p>
     <Link to="/about/">about me &rArr;</Link>
-  </>
+  </Layout>
 )
