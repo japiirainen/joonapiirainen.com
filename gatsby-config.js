fa5e998752b7b3256588fb8d3c5044734e772fea
unknown <joona.piirainen@gmail.com>
2020-05-14 10:51:57 +0300

    post preview at index.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,5 +22,12 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "posts",
+        path: "posts",
+      },
+    },
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,20 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "./components/layout"
+import PostPreview from "./components/post-preview"
+import usePosts from "./hooks/use-posts"
 
-export default () => (
+export default () => {
+const posts = usePosts();
+
+return (
   <Layout>
     <h1>Hello All!</h1>
     <p>Good morning people</p>
     <Link to="/about/">about me &rArr;</Link>
+    {posts.map(post => (
+      <PostPreview key={post.slug} post={post} />
+    ))}
   </Layout>
 )
+}
