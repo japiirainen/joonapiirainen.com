f9e32fcd8c5725ff1acf84cdce2b6a49bdb27926
Joona Piirainen <joona.piirainen@gmail.com>
2020-10-05 08:25:38 +0300

    remove dark mode

--- src/pages/components/header.js
+++ src/pages/components/header.js
@@ -1,70 +1,68 @@
-import React from 'react'
-import styled from '@emotion/styled'
-import { css } from '@emotion/core'
-import { Link } from 'gatsby'
-import DarkModeToggle from './DarkModeToggle'
+import React from "react"
+import styled from "@emotion/styled"
+import { css } from "@emotion/core"
+import { Link } from "gatsby"
 
 const NavLink = styled(Link)`
-    text-decoration: none;
-    font-size: 1rem;
-    font-weight: ${(props) => props.fontWeight || 'normal'};
-    line-height: 1;
-    margin: 0 0.5rem 0 0;
-    padding: 0.25rem;
+  text-decoration: none;
+  font-size: 1rem;
+  font-weight: ${props => props.fontWeight || "normal"};
+  line-height: 1;
+  margin: 0 0.5rem 0 0;
+  padding: 0.25rem;
 
-    &.current-page {
-        border-bottom: 2px solid #222;
-    }
+  &.current-page {
+    border-bottom: 2px solid #222;
+  }
 
-    &:last-of-type {
-        margin-right: 0;
-    }
+  &:last-of-type {
+    margin-right: 0;
+  }
 `
 
 const Header = () => (
-    <header
-        css={css`
-            border-bottom: 1.5px dashed #555;
-            display: flex;
-            justify-content: space-between;
-            padding: 0.5rem calc((100vw - 550px - 0.5rem) / 2);
+  <header
+    css={css`
+      border-bottom: 1.5px dashed #555;
+      display: flex;
+      justify-content: space-between;
+      padding: 0.5rem calc((100vw - 550px - 0.5rem) / 2);
 
-            .jp {
-                font-size: 1.3rem;
+      .jp {
+        font-size: 1.3rem;
 
-                @media (max-width: 850px) {
-                    font-size: 0.9rem;
-                }
-            }
-            .contact {
-                margin: 1rem;
+        @media (max-width: 850px) {
+          font-size: 0.9rem;
+        }
+      }
+      .contact {
+        margin: 1rem;
 
-                @media (max-width: 850px) {
-                    font-size: 0.8rem;
-                }
-            }
-        `}
+        @media (max-width: 850px) {
+          font-size: 0.8rem;
+        }
+      }
+    `}
+  >
+    <NavLink to="/" fontWeight="bold" className="jp">
+      joonapiirainen.com
+    </NavLink>
+
+    <nav
+      css={css`
+        margin-top: 0;
+      `}
     >
-        <DarkModeToggle />
-        <NavLink to="/" fontWeight="bold" className="jp">
-            joonapiirainen.com
-        </NavLink>
-
-        <nav
-            css={css`
-                margin-top: 0;
-            `}
-        >
-            <NavLink
-                to="/contact/"
-                activeClassName="current-page"
-                className="contact"
-            >
-                Contact
-            </NavLink>
-        </nav>
-        {}
-    </header>
+      <NavLink
+        to="/contact/"
+        activeClassName="current-page"
+        className="contact"
+      >
+        Contact
+      </NavLink>
+    </nav>
+    {}
+  </header>
 )
 
 export default Header
--- src/pages/components/layout.js
+++ src/pages/components/layout.js
@@ -1,95 +1,86 @@
-import React from 'react'
-import { Global, css } from '@emotion/core'
-import { Helmet } from 'react-helmet'
-import Header from './header'
-import Footer from './Footer'
+import React from "react"
+import { Global, css } from "@emotion/core"
+import { Helmet } from "react-helmet"
+import Header from "./header"
+import Footer from "./Footer"
 
 const Layout = ({ children }) => {
-    return (
-        <>
-            <Global
-                styles={css`
-                    * {
-                        margin: 0;
-                    }
+  return (
+    <>
+      <Global
+        styles={css`
+          * {
+            margin: 0;
+          }
 
-                    * + * {
-                        margin-top: 1rem;
-                    }
+          * + * {
+            margin-top: 1rem;
+          }
 
-                    html,
-                    body {
-                        margin: 0;
-                        font-family: 'DM Mono', monospace;
-                        font-size: 20px;
-                        line-height: 1.5;
-                        min-height: calc(100vh - 211px -58px);
+          html,
+          body {
+            margin: 0;
+            font-family: "DM Mono", monospace;
+            font-size: 20px;
+            line-height: 1.5;
+            min-height: calc(100vh - 211px -58px);
 
-                        /*remove margin for main div gatsby mounts to.*/
-                        > div {
-                            margin-top: 0;
-                        }
-                        body.light-mode {
-                            background-color: #fff;
-                            color: #333;
-                            transition: background-color 0.3s ease;
+            /*remove margin for main div gatsby mounts to.*/
+            > div {
+              margin-top: 0;
+            }
+            body {
+              background-color: #fff;
+              color: #333;
+              transition: background-color 0.3s ease;
 
-                            a {
-                                color: #333;
-                            }
-                        }
-                        body.dark-mode {
-                            background-color: #121214;
-                            color: #dfdfdf;
-                            transition: background-color 0.3s ease;
+              a {
+                color: #333;
+              }
+            }
 
-                            a {
-                                color: #dfdfdf;
-                            }
-                        }
+            h1,
+            h2,
+            h3,
+            h4,
+            h5,
+            h6 {
+              line-height: 1.1;
 
-                        h1,
-                        h2,
-                        h3,
-                        h4,
-                        h5,
-                        h6 {
-                            line-height: 1.1;
+              + * {
+                margin-top: 0.5rem;
+              }
+            }
+            strong {
+            }
 
-                            + * {
-                                margin-top: 0.5rem;
-                            }
-                        }
-                        strong {
-                        }
-
-                        li {
-                            margin-top: 0.25rem;
-                        }
-                    }
-                    h2 {
-                        margin: 2.5rem auto 1.5rem auto;
-                    }
-                `}
-            />
-            <Helmet>
-                <html lang="en" />
-                <title>Joonapiirainen</title>
-                <meta name="description" content="My new personal site." />
-            </Helmet>
-            <Header />
-            <main
-                css={css`
-                    margin: 2rem auto 4rem;
-                    max-width: 90vw;
-                    width: 600px;
-                `}
-            >
-                {children}
-            </main>
-            <Footer />
-        </>
-    )
+            li {
+              margin-top: 0.25rem;
+            }
+          }
+          h2 {
+            margin: 2.5rem auto 1.5rem auto;
+          }
+        `}
+      />
+      <Helmet>
+        <html lang="en" />
+        <title>Joonapiirainen</title>
+        <meta name="description" content="My new personal site." />
+      </Helmet>
+      <Header />
+      <main
+        css={css`
+          margin: 2rem auto 4rem;
+          max-width: 90vw;
+          width: 600px;
+        `}
+      >
+        {children}
+      </main>
+      <Footer />
+    </>
+  )
 }
 
 export default Layout
